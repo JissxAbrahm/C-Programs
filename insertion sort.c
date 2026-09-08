5c00a63c3f9650c1bf193cb00b267c761ba9b66e
JissxAbraham <90022589+JissxAbrahm@users.noreply.github.com>
2026-09-08 21:52:42 +0530

    Add files via upload

---
+++ insertion sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void insertionSort(int a[], int n)
+{
+    int i, j, key;
+
+    for (i = 1; i < n; i++)
+    {
+        key = a[i];
+        j = i - 1;
+
+        while (j >= 0 && a[j] > key)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        a[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int a[] = {5, 3, 8, 4, 2};
+    int n = 5;
+    int i;
+
+    insertionSort(a, n);
+
+    printf("Sorted array: ");
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
